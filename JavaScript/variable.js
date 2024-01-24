1ba292c549fc4e65c2db74ada4e6c9a4b1658670
Ayush-birla <125186167+Ayush-birla@users.noreply.github.com>
2024-01-24 06:26:29 +0000

    create variables

---
+++ JavaScript/variable.js
@@ -0,0 +1,12 @@
+console.log("hii");
+const accountId=133
+let accountEmail="ayu@gmail.com"
+var accountPass="123"
+accountCity="Indore"
+// accountId=90
+accountEmail="raju@gmail.com"
+accountPass="raju@gmail.com"
+
+/* prefer not to use var
+because of issues in block scop and functional scope */
+console.table([accountId,accountEmail,accountPass,accountCity])
