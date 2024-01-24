9748353586aa3051aed20a64f4d9717ba4d7cfae
Ayush-birla <125186167+Ayush-birla@users.noreply.github.com>
2024-01-24 07:13:26 +0000

    conversion

---
+++ JavaScript/converstion.js
@@ -0,0 +1,26 @@
+let number="";
+let Numberofnumber=Number(number);
+console.log(typeof Numberofnumber);
+console.log( Numberofnumber);
+
+/*
+    33a->NaN
+    "33"->33
+    ""->0
+    undefined->NaN
+    null->0
+*/
+
+let islog=""
+let bIsLog=Boolean(islog);
+console.log(typeof bIsLog);
+console.log(bIsLog);
+
+/*
+    1->true
+    0->false
+    "a"->true
+    ""->false
+    null->false
+    undefined->false
+ */
