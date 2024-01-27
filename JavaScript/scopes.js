dafdd498b738aaa38628ee4649320a0a32e1d288
Ayush-birla <125186167+Ayush-birla@users.noreply.github.com>
2024-01-27 04:44:49 +0000

    scopes update

---
+++ JavaScript/scopes.js
@@ -0,0 +1,35 @@
+let a=90
+if (true) {
+    let a=8;
+    const b=3
+    var c=0
+console.log(a);
+
+}
+// console.log(a);
+// console.log(c);
+
+function one(){
+    username="ayush"
+    if(true)
+    {
+        const website="redit"
+        console.log(this.username);
+    }
+    // console.log(website);
+}
+one()
+
+console.log(addOne(8));
+function addOne(num)
+{
+    return num+1
+}
+
+// console.log(ans);
+ans(8)
+const ans=function(num){
+    return num+1
+}
+
+
